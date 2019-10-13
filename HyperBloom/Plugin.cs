c50c5f0a8cc5a91ea5769a92a4415af425cfa92a
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2019-10-13 03:12:10 -0500

    Generate and load presets.

--- HyperBloom/Plugin.cs
+++ HyperBloom/Plugin.cs
@@ -1,14 +1,17 @@
 ﻿using IPA;
 using UnityEngine.SceneManagement;
 using IPALogger = IPA.Logging.Logger;
+using UnityEngine;
+using System.Linq;
+using IPA.Utilities;
 
 namespace HyperBloom
 {
     public class Plugin : IBeatSaberPlugin
     {
         internal static BS_Utils.Utilities.Config config;
-        static readonly string configName = "CustomDebrisSettings";
-        static readonly string sectionParticles = "Settings";
+        static readonly string configName = "HyperBloom";
+        static readonly string sectionName = "Settings";
 
         private bool postProcessEnabled = true;
         private float baseColorBoost = 0.5f;
@@ -21,6 +24,7 @@ namespace HyperBloom
         public void Init(IPALogger logger)
         {
             Logger.logger = logger;
+            config = new BS_Utils.Utilities.Config(configName);
         }
 
         public void OnApplicationStart() { }
@@ -31,7 +35,12 @@ namespace HyperBloom
 
         public void OnUpdate() { }
 
-        public void OnActiveSceneChanged(Scene prevScene, Scene nextScene) { }
+        public void OnActiveSceneChanged(Scene prevScene, Scene nextScene)
+        {
+            var preset = GeneratePreset();
+            var camera = Resources.FindObjectsOfTypeAll<MainEffect>().FirstOrDefault();
+            camera?.GetPrivateField<MainEffectParams>("_mainEffectParams")?.InitFromPreset(preset);
+        }
 
         public void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode) { }
 
@@ -39,7 +48,29 @@ namespace HyperBloom
 
         private MainEffectGraphicsSettingsPresets.Preset GeneratePreset()
         {
+            LoadSettings();
 
+            var preset = new MainEffectGraphicsSettingsPresets.Preset
+            {
+                postProcessEnabled = postProcessEnabled,
+                baseColorBoost = baseColorBoost,
+                baseColorBoostThreshold = baseColorBoostThreshold,
+                bloomIterations = bloomIterations,
+                bloomIntensity = bloomIntensity,
+                textureWidth = textureWidth
+            };
+
+            return preset;
+        }
+
+        private void LoadSettings()
+        {
+            postProcessEnabled = config.GetBool(sectionName, "Post Process Enabled", true, true);
+            baseColorBoost = config.GetFloat(sectionName, "Base Color Boost", 0.5f, true);
+            baseColorBoostThreshold = config.GetFloat(sectionName, "Base Color Boost Threshold", 0.1f, true);
+            bloomIterations = config.GetInt(sectionName, "Bloom Iterations", 4, true);
+            bloomIntensity = config.GetFloat(sectionName, "Bloom Intensity", 1, true);
+            textureWidth = config.GetInt(sectionName, "Texture Width", 512, true);
         }
     }
 }
